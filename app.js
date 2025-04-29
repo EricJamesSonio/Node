c477d5cb890871e7935b893d713486cba307156f
EricJames <ericjamessonio7@gmail.com>
2025-04-28 17:01:12 -0800

    "Nothing"

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
+const os = require('os');
 
-sayHello('Eric');
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Total Memory : ${freeMemory}`);
